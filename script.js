a6077db62f5df3e68552813866bb2405712014be
1301235340-commits <1301235340@s.tusd1.org>
2026-05-04 21:32:05 +0000

    quick

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 console.log("Script started");
 
+let clickCount = 0;
+const clickButton = document.getElementById('clickButton');
+
+clickButton.addEventListener('click', function() {
+    clickCount++;
+    console.log('Clicks: ' + clickCount);
+    clickButton.textContent = clickCount;
+});
+
