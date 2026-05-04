f2ff5fd6efed8e5321f6d2cc71192ec39cdaacfe
1301235340-commits <1301235340@s.tusd1.org>
2026-05-04 21:49:34 +0000

    day 2

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ console.log("Script started");
 
 let clickCount = 0;
 const clickButton = document.getElementById('clickButton');
+const statsDiv = document.getElementById('stats');
 
 clickButton.addEventListener('click', function() {
     clickCount++;
     console.log('Clicks: ' + clickCount);
     clickButton.textContent = clickCount;
+    statsDiv.textContent = 'Clicks: ' + clickCount;
 });
 
